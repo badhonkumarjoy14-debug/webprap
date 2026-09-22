b94b7b6d5134f21428bd3aaa97fe9de472b002c2
joy <you@example.com>
2026-09-22 10:28:40 +0900

    adding new files

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 
+let name = document.querySelector("#name");
+let job = document.querySelector("#job");
+let bio = document.querySelector("#bio");
+let pic = document.querySelector("#pic");
+
+let form = document.querySelector("form");
+
+let cardname = document.querySelector("#username");
+let imgg = document.querySelector("#imgg");
+let title = document.querySelector("#title");
+let detailss = document.querySelector("#details");
+
+form.addEventListener("submit", function(details){
+    details.preventDefault();
+    
+    cardname.textContent= details.srcElement[0].value
+    title.textContent = details.srcElement[1].value
+    detailss.textContent = details.srcElement[2].value
+    imgg. innerHTML = `<img src=details.srcElement[2].value alt="">`
+});
